637b01c42d47f2a9a6349098a92a8c5d0dac99bd
sofianeazogagh <sofiane.a.f@hotmail.fr>
2023-02-01 19:07:56 -0500

    Add MultiBAAcc for perf on Alien

--- packed-PROBONITE/src/MultiBAAcc.cpp
+++ packed-PROBONITE/src/MultiBAAcc.cpp
@@ -1,10 +1,21 @@
+#include <iostream>
+#include <string>
+#include <fstream>
+
 #include "openfhe.h"
 using namespace lbcrypto;
 using namespace std;
 
+#include "ciphertext-ser.h"
+#include "cryptocontext-ser.h"
+#include "key/key-ser.h"
+#include "scheme/bfvrns/bfvrns-ser.h"
+
 
 #include "sys/time.h"
 
+
+const std::string DATAFOLDER = "../demoData";
 
 
 
@@ -16,6 +27,45 @@ static double getMillies(timeval timestart, timeval timeend)
 return (double)(time2-time1)/1000;
 }
 
+CryptoContext<DCRTPoly> Deserialization(PublicKey<DCRTPoly> *pk, PrivateKey<DCRTPoly> *sk)
+{
+   CryptoContext<DCRTPoly> cc;
+    if (!Serial::DeserializeFromFile(DATAFOLDER + "/cryptocontext.txt", cc, SerType::BINARY)) {
+        std::cerr << "I cannot read serialization from " << DATAFOLDER + "/cryptocontext.txt" << std::endl;
+    }
+    std::cout << "The cryptocontext has been deserialized." << std::endl;
+
+    if (Serial::DeserializeFromFile(DATAFOLDER + "/key-public.txt", *pk, SerType::BINARY) == false) {
+        std::cerr << "Could not read public key" << std::endl;
+    }
+    std::cout << "The public key has been deserialized." << std::endl;
+
+    std::ifstream emkeys(DATAFOLDER + "/key-eval-mult.txt", std::ios::in | std::ios::binary);
+    if (!emkeys.is_open()) {
+        std::cerr << "I cannot read serialization from " << DATAFOLDER + "/key-eval-mult.txt" << std::endl;
+    }
+    if (cc->DeserializeEvalMultKey(emkeys, SerType::BINARY) == false) {
+        std::cerr << "Could not deserialize the eval mult key file" << std::endl;
+    }
+    std::cout << "Deserialized the eval mult keys." << std::endl;
+
+    std::ifstream erkeys(DATAFOLDER + "/key-eval-rot.txt", std::ios::in | std::ios::binary);
+    if (!erkeys.is_open()) {
+        std::cerr << "I cannot read serialization from " << DATAFOLDER + "/key-eval-rot.txt" << std::endl;
+    }
+    if (cc->DeserializeEvalAutomorphismKey(erkeys, SerType::BINARY) == false) {
+        std::cerr << "Could not deserialize the eval rotation key file" << std::endl;
+    }
+    std::cout << "Deserialized the eval rotation keys." << std::endl;
+
+    if (Serial::DeserializeFromFile(DATAFOLDER + "/key-private.txt", *sk, SerType::BINARY) == false) {
+        std::cerr << "Could not read secret key" << std::endl;
+    }
+    std::cout << "The secret key has been deserialized." << std::endl;
+
+    return cc;
+    printf("DEBUG\n");
+}
 
 
 Ciphertext<DCRTPoly> RotateAndSum(Ciphertext<DCRTPoly> ct, CryptoContext<DCRTPoly> cc)
@@ -158,9 +208,10 @@ Ciphertext<DCRTPoly> MultiOHS(Ciphertext<DCRTPoly> ct_I, int max_slots, int numb
 void UniTESTMultiOHS(CryptoContext<DCRTPoly> cc, KeyPair<DCRTPoly> keyPair, int max_slots, int number_of_clients, vector<int64_t> clear_I)
 {
 
+
+  ofstream fichier("../perf.txt", ios::out | ios::app);
   Plaintext pt_I = cc->MakePackedPlaintext(clear_I);
   auto ct_I = cc->Encrypt(keyPair.publicKey, pt_I);
-
 
   vector<int64_t> vecT(cc->GetRingDimension());
   for (int i = 0; i < number_of_clients; i++)
@@ -172,13 +223,20 @@ void UniTESTMultiOHS(CryptoContext<DCRTPoly> cc, KeyPair<DCRTPoly> keyPair, int 
 
   vector<int64_t> oneN(cc->GetRingDimension(), 1);
   Plaintext ptoneN = cc->MakePackedPlaintext(oneN);
-
+  timeval t_start_ohs, t_end_ohs;
+  gettimeofday(&t_start_ohs, NULL);
   auto ct_OHS = MultiOHS(ct_I,max_slots,number_of_clients,cc, ptT, ptoneN);
+  gettimeofday(&t_end_ohs, NULL);
+  fichier << "(" << max_slots << "," << clear_I.size() <<") = " ; 
+  fichier << std::setprecision(5) << getMillies(t_start_ohs, t_end_ohs) << " ms" << endl;
+  
 
-  Plaintext result;
-  cc->Decrypt(keyPair.secretKey, ct_OHS, &result);
-  cout << "Input " << pt_I << endl;
-  cout << "Ouput " << result << endl;
+  // Plaintext result;
+  // cc->Decrypt(keyPair.secretKey, ct_OHS, &result);
+  // cout << "Input " << pt_I << endl;
+  // cout << "Ouput " << result << endl;
+
+  fichier.close();
 
 }
 
@@ -203,34 +261,33 @@ int main() {
   timeval t_start, t_end;
 
   
-  int N = std::pow(2, 15);
-  int t = 2 * N + 1;  // 65537;
-  uint32_t depth = 17;
-  double sigma = 3.2;
-  int m = 8;
+  // int N = std::pow(2, 15);
+  // int t = 2 * N + 1;  // 65537;
+  // uint32_t depth = 17;
+  // double sigma = 3.2;
 
 //   SecurityLevel securityLevel = HEStd_128_classic;
-  CCParams<CryptoContextBFVRNS> parameters;
+  // CCParams<CryptoContextBFVRNS> parameters;
 
-  parameters.SetPlaintextModulus(t);
-  parameters.SetStandardDeviation(sigma);
-  parameters.SetMultiplicativeDepth(depth);
-  parameters.SetMaxRelinSkDeg(2);
-  parameters.SetScalingModSize(60);
-  parameters.SetRingDim(N);
+  // parameters.SetPlaintextModulus(t);
+  // parameters.SetStandardDeviation(sigma);
+  // parameters.SetMultiplicativeDepth(depth);
+  // parameters.SetMaxRelinSkDeg(2);
+  // parameters.SetScalingModSize(60);
+  // parameters.SetRingDim(N);
   
-  cout << "Generating the Crypto Context.." << endl;
-  CryptoContext<DCRTPoly> cc = GenCryptoContext(parameters);
-  cc->Enable(PKE);
-  cc->Enable(ADVANCEDSHE);
-  cc->Enable(KEYSWITCH);
-  cc->Enable(LEVELEDSHE);
+  // cout << "Generating the Crypto Context.." << endl;
+  // CryptoContext<DCRTPoly> cc = GenCryptoContext(parameters);
+  // cc->Enable(PKE);
+  // cc->Enable(ADVANCEDSHE);
+  // cc->Enable(KEYSWITCH);
+  // cc->Enable(LEVELEDSHE);
 
 
-  cout << "Generating the KeyPair.." << endl;
-  KeyPair<DCRTPoly> keyPair;
-  keyPair = cc->KeyGen();
-  cc->EvalMultKeyGen(keyPair.secretKey);
+  // cout << "Generating the KeyPair.." << endl;
+  // KeyPair<DCRTPoly> keyPair;
+  // keyPair = cc->KeyGen();
+  // cc->EvalMultKeyGen(keyPair.secretKey);
 
 
   // cout << "Generating the Rotations Keys.." << endl;
@@ -238,23 +295,37 @@ int main() {
   // for(int i = 0; i < log2(N); i++){indexList.push_back(pow(2,i));}
   // cc->EvalRotateKeyGen(keyPair.secretKey, indexList);
 
-
-  vector<int64_t> clear_I = {2,1,4,5}; // indices des features des differents clients  
-  int max_slots = *max_element(clear_I.begin(), clear_I.end()) + 1; // nombre de feature max
-  int number_of_clients = clear_I.size();
-
-  cout << "Generating the Rotations Keys.." << endl;
-  std::vector<int> indexList2;
-  for(int i = 1; i < number_of_clients ; i++){indexList2.push_back(-i*max_slots);}
-  for(int i = 0; i < number_of_clients ; i++){indexList2.push_back(i*max_slots);}
-  for(int i = 1; i < number_of_clients ; i++){indexList2.push_back(-i);}
-  for(int i = 0; i < number_of_clients ; i++){indexList2.push_back(i);}
-  for(int i = 1; i < max_slots ; i++){indexList2.push_back(-i);}
-  for(int i = 0; i < max_slots ; i++){indexList2.push_back(i);}
-  cc->EvalRotateKeyGen(keyPair.secretKey, indexList2);
-
+  // cout << "Generating the Rotations Keys.." << endl;
+  // std::vector<int> indexList2;
+  // for(int i = 1; i < number_of_clients ; i++){indexList2.push_back(-i*max_slots);}
+  // for(int i = 0; i < number_of_clients ; i++){indexList2.push_back(i*max_slots);}
+  // for(int i = 1; i < number_of_clients ; i++){indexList2.push_back(-i);}
+  // for(int i = 0; i < number_of_clients ; i++){indexList2.push_back(i);}
+  // for(int i = 1; i < max_slots ; i++){indexList2.push_back(-i);}
+  // for(int i = 0; i < max_slots ; i++){indexList2.push_back(i);}
+  // cc->EvalRotateKeyGen(keyPair.secretKey, indexList2);
   // UniTESTConcatDeconcat(number_of_clients,max_slots,cc,keyPair);
+
+
+
+
+
+  PublicKey<DCRTPoly> pk;
+  PrivateKey<DCRTPoly> sk;
+  CryptoContext<DCRTPoly> cc = Deserialization(&pk,&sk);
+  KeyPair<DCRTPoly> keyPair(pk,sk);
+
+  // vector<int64_t> clear_I = {2,1,4,5}; // indices des features des differents clients
+  int number_of_clients_max = 17; // 100 sur Alien
+  for (int i = 3; i < number_of_clients_max; i++)
+  {
+  vector<int64_t> clear_I(i);
+  iota(clear_I.begin(), clear_I.end(),1);
+  int max_slots = 6; //100 sur Alien
+  // int max_slots = *max_element(clear_I.begin(), clear_I.end()) + 1; // nombre de feature max
+  int number_of_clients = clear_I.size();
   UniTESTMultiOHS(cc, keyPair, max_slots, number_of_clients, clear_I);
+  }
 
 
   // Plaintext pt_I = cc->MakePackedPlaintext(clear_I);
