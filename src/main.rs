aad4e1b01b9d86477289f75ca2e028b189eda520
Sofiane A. <sofiane@cardinal.dev-null.ca>
2025-12-22 13:00:00 -0500

    ready for xp

--- src/main.rs
+++ src/main.rs
@@ -9,40 +9,47 @@ use model::*;
 use revolut::{key, Context};
 use tfhe::shortint::parameters::*;
 
-const GENERATE_TREE: bool = false;
+const GENERATE_TREE: bool = true;
 
 fn main() {
-    let mut ctx = Context::from(PARAM_MESSAGE_3_CARRY_0);
+    let mut ctx = Context::from(PARAM_MESSAGE_4_CARRY_0);
     let private_key = key(ctx.parameters());
     let public_key = &private_key.public_key;
+    let p = ctx.full_message_modulus() as u64;
 
     let mut tree: Tree;
-    let tree_depth = 3;
+    // let tree_depth = 3;
     let n_classes = 2;
 
-    if GENERATE_TREE {
-        tree = Tree::generate_random_tree(tree_depth, n_classes, &ctx);
-        tree.save_to_file(
-            &format!("random_trees/random_tree_{}_{}.json", tree_depth, n_classes),
-            &ctx,
-        );
-    } else {
-        tree = Tree::load_from_file(
-            &format!("random_trees/random_tree_{}_{}.json", tree_depth, n_classes),
-            &ctx,
-        )
-        .unwrap();
+    for tree_depth in [3,8,10].iter() {
+        println!("Tree depth: {}", *tree_depth);
+        println!("--------------------------------");
+        if GENERATE_TREE {
+            tree = Tree::generate_random_tree(*tree_depth, n_classes, &ctx);
+            tree.save_to_file(
+                &format!("random_trees/exp/random_tree_{}_{}.json", *tree_depth, n_classes),
+                &ctx,
+            );
+        } else {
+            tree = Tree::load_from_file(
+                &format!("random_trees/exp/random_tree_{}_{}.json", *tree_depth, n_classes),
+                &ctx,
+            )
+            .unwrap();
+        }
+
+        let feature_vector: Vec<u64> = (0..p)
+            .map(|_| rand::random::<u64>() % p)
+            .collect();
+        let class = 1;
+
+        let query = Query::make_query(&feature_vector, &class, &private_key, &mut ctx);
+
+        let start = Instant::now();
+        probonite(&mut tree, &query, &public_key, &ctx);
+        let end = Instant::now();
+        println!("Time taken: {:?}", end.duration_since(start));
+
+        tree.print_tree(&private_key, &ctx);
     }
-
-    let feature_vector = vec![1, 0, 1, 1, 1, 1, 0, 1];
-    let class = 1;
-
-    let query = Query::make_query(&feature_vector, &class, &private_key, &mut ctx);
-
-    let start = Instant::now();
-    probonite(&mut tree, &query, &public_key, &ctx);
-    let end = Instant::now();
-    println!("Time taken: {:?}", end.duration_since(start));
-
-    tree.print_tree(&private_key, &ctx);
 }
